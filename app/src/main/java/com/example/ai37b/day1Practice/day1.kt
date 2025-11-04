fcfe2c8b583313f2db3ef1ad060ccbc0a6661d96
Aayush-cod <aayush0510bohara@gmail.com>
2025-11-04 14:37:06 +0545

    Practice-Kotlin

---
+++ app/src/main/java/com/example/ai37b/day1Practice/day1.kt
@@ -0,0 +1,60 @@
+package com.example.ai37b.day1Practice
+
+fun main() {
+//    println("Hello")
+//    print("world")
+
+//    mutable
+//    var name : String = "sandis"
+//
+//
+////    immutable
+//    val age : Int = 15
+//
+//    println("My name is $name  and  age is $age")
+//
+//    println("My name is ${name.uppercase()} and  age is $age")
+
+
+//    ARRAY/ array
+
+//    val age = arrayOf(10,20,30)    //static array
+//    age[2] = 50
+//    println(age[2])
+//
+//
+//    val age1 = ArrayList<Int>() //value initialize garna mildaina
+//    val age2 = arrayListOf<Int>(10,20,30)
+//
+//    age1.add(5)
+//
+//    age2.add(10)
+//
+////    For Loop
+//
+//for (i in 0 .. 10 step 2){
+//    print(i)
+//
+//}
+//    for (i in 0 until 10 step 2){
+//        print(i)
+//
+//    }
+//
+//    for (i in 10 downTo 0){
+//        print(i)
+//    }
+
+    val result = sum(10,20)
+
+}
+
+//    Functions
+
+fun sum(a : Int , b: Int) : Int{
+    return a + b;
+}
+
+fun login(username : String, password: String) :Unit{
+
+}
---
+++ app/src/main/java/com/example/ai37b/day1Practice/variable_use.kt
@@ -0,0 +1,16 @@
+package com.example.ai37b.day1Practice
+
+fun main(){
+    //mutable
+//we can re assign values to mutable variables
+    var age = 20;
+    age = 25
+//immutable
+//we can't re assign values to immutable variables
+    val roll = 15
+//    roll = 10   -- value can't be in val
+
+    println(age)
+    print(roll)
+
+}
