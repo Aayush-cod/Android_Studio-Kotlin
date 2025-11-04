536beaf2261619dbb4dc10a6def3415189b7d561
Aayush-cod <aayush0510bohara@gmail.com>
2025-11-04 14:48:52 +0545

    extra practice

---
+++ app/src/main/java/com/example/ai37b/day1Practice/typeConversion.kt
@@ -0,0 +1,18 @@
+package com.example.ai37b.day1Practice
+
+fun main(){
+
+    var x: Double = 132.32
+
+    var y: Int = x.toInt()
+
+    var z: Byte = y.toByte()
+
+
+    println(x)
+
+    println(y)
+    
+    println(z)
+
+}
